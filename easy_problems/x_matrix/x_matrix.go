a22e4cdd32face631d503bf0cd9b82453b53bec9
Archit Agarwal <architagr@gmail.com>
2022-07-08 22:15:16 +0530

    2319(easy): Check if Matrix Is X-Matrix

---
+++ easy_problems/x_matrix/x_matrix.go
@@ -0,0 +1,22 @@
+package x_matrix
+
+func checkXMatrix(grid [][]int) bool {
+	n := len(grid)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if i == j || i+j == n-1 {
+				if grid[i][j] == 0 {
+					return false
+				}
+
+			} else {
+				if grid[i][j] != 0 {
+					return false
+				}
+
+			}
+		}
+	}
+
+	return true
+}
