d746ce36cffcb04b535e783c9cae8464d434f5b3
Archit Agarwal <architagr@gmail.com>
2022-07-10 23:16:57 +0530

    509(easy): Fibonacci Number

---
+++ easy_problems/fibonacci_number/fibonacci_number.go
@@ -0,0 +1,17 @@
+package fibonacci_number
+
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+
+	a, b, c := 0, 1, 0
+
+	for i := 2; i <= n; i++ {
+		c = a + b
+		a = b
+		b = c
+	}
+
+	return c
+}
