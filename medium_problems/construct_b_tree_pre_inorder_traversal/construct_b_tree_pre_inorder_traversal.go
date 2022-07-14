d88aec6f9d702ec04e0f80894925d16e87fb3a1c
Archit Agarwal <architagr@gmail.com>
2022-07-15 01:48:52 +0530

    105(medium): Construct Binary Tree from Preorder and Inorder Traversal

---
+++ medium_problems/construct_b_tree_pre_inorder_traversal/construct_b_tree_pre_inorder_traversal.go
@@ -0,0 +1,36 @@
+package construct_b_tree_pre_inorder_traversal
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func buildNode(value int) *TreeNode {
+	node := new(TreeNode)
+	node.Val = value
+
+	return node
+}
+func BuildBinaryTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+	leftnodeIndex := 0
+	leftNodeValue := preorder[0]
+	root := buildNode(leftNodeValue)
+	leninOrder := len(inorder)
+	for i := 0; i < leninOrder; i++ {
+		if inorder[i] == leftNodeValue {
+			leftnodeIndex = i
+			break
+		}
+	}
+	if len(preorder) > 0 {
+		root.Left = BuildBinaryTree(preorder[1:leftnodeIndex+1], inorder[:leftnodeIndex])
+	}
+	if leninOrder > 0 {
+		root.Right = BuildBinaryTree(preorder[leftnodeIndex+1:], inorder[leftnodeIndex+1:])
+	}
+	return root
+}
---
+++ medium_problems/construct_b_tree_pre_inorder_traversal/construct_b_tree_pre_inorder_traversal_test.go
@@ -0,0 +1,51 @@
+package construct_b_tree_pre_inorder_traversal
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+type TestCase struct {
+	inputA, inputB []int
+	expected       []int
+}
+
+func TestBuildBinaryTree(t *testing.T) {
+	testCases := make([]TestCase, 0)
+	testCases = append(testCases, TestCase{
+		inputA:   []int{1, 2, 4, 3, 5, 7, 6, 8, 9},
+		inputB:   []int{4, 2, 1, 7, 5, 3, 8, 6, 9},
+		expected: []int{1, 2, 4, 3, 5, 7, 6, 8, 9},
+	})
+	testCases = append(testCases, TestCase{
+		inputA:   []int{1, 2, 3, 4, 5},
+		inputB:   []int{1, 2, 3, 4, 5},
+		expected: []int{1, 2, 3, 4, 5},
+	})
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprintf("testing number %d", (i+1)), func(test *testing.T) {
+			got := BuildBinaryTree(testCase.inputA, testCase.inputB)
+			x := PreorderTraversal(got)
+			if !reflect.DeepEqual(x, testCase.expected) {
+				test.Fatalf("tested %d expected %+v but got %+v", (i + 1), testCase.expected, got)
+			}
+		})
+	}
+}
+
+func PreorderTraversal(A *TreeNode) []int {
+	arr := make([]int, 0)
+	return traversal(A, arr)
+}
+
+func traversal(A *TreeNode, arr []int) []int {
+	if A == nil {
+		return arr
+	}
+	arr = append(arr, A.Val)
+	arr = traversal(A.Left, arr)
+	arr = traversal(A.Right, arr)
+
+	return arr
+}
