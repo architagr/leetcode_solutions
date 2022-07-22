2e4c8785acfd324aaeed04d8cba448bcb6bedb2b
Archit Agarwal <architagr@gmail.com>
2022-07-23 02:23:23 +0530

    2161(medium): Partition Array According to Given Pivot

--- medium_problems/partition_array_according_given_pivot/partition_array_according_given_pivot.go
+++ medium_problems/partition_array_according_given_pivot/partition_array_according_given_pivot.go
@@ -3,16 +3,17 @@ package partition_array_according_given_pivot
 func PivotArray(nums []int, pivot int) []int {
 	n := len(nums)
 	left := make([]int, 0, n)
+	arr := make([]int, 0, n)
 	right := make([]int, 0, n)
 
 	for i := 0; i < n; i++ {
 		if nums[i] < pivot {
 			left = append(left, nums[i])
 		} else if nums[i] == pivot {
-			right = append([]int{nums[i]}, right...)
+			arr = append(arr, nums[i])
 		} else {
 			right = append(right, nums[i])
 		}
 	}
-	return append(left, right...)
+	return append(left, append(arr, right...)...)
 }
