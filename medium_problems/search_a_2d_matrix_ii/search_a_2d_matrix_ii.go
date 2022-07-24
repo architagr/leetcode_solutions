f7b7f9f97cf1e6f20dcb6ca4b08f1f6fd1849b79
Archit Agarwal <architagr@gmail.com>
2022-07-24 20:06:21 +0530

    240(medium): Search a 2D Matrix II

---
+++ medium_problems/search_a_2d_matrix_ii/search_a_2d_matrix_ii.go
@@ -0,0 +1,16 @@
+package search_a_2d_matrix_ii
+
+func SearchMatrix(matrix [][]int, target int) bool {
+	rows, cols := len(matrix), len(matrix[0])
+	i, j := rows-1, 0
+	for i >= 0 && j < cols {
+		if matrix[i][j] < target {
+			j++
+		} else if matrix[i][j] > target {
+			i--
+		} else {
+			return true
+		}
+	}
+	return false
+}
---
+++ medium_problems/search_a_2d_matrix_ii/search_a_2d_matrix_ii_test.go
@@ -0,0 +1,34 @@
+package search_a_2d_matrix_ii
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+type TestCase struct {
+	input    [][]int
+	target   int
+	expected bool
+}
+
+var testCases []TestCase = []TestCase{
+	{[][]int{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 0, false},
+	{[][]int{{5, 6, 9}, {9, 10, 11}, {11, 14, 18}}, 9, true},
+	{[][]int{{2, 5}, {2, 8}, {7, 9}, {7, 11}, {9, 11}}, 7, true},
+	{[][]int{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 5, true},
+	{[][]int{{1, 4, 7, 11, 15}}, 5, false},
+	{[][]int{{1}, {2}, {3}, {10}, {18}}, 5, false},
+	{[][]int{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 20, false},
+}
+
+func TestSearchMatrix(t *testing.T) {
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprintf("testing number %d", (i+1)), func(test *testing.T) {
+			got := SearchMatrix(testCase.input, testCase.target)
+			if !reflect.DeepEqual(got, testCase.expected) {
+				test.Fatalf("tested %d expected %+v bti got %+v", (i + 1), testCase.expected, got)
+			}
+		})
+	}
+}
