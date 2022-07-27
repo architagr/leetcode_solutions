89b067dcbf3175711b80978c0a304fca3e34f156
Archit Agarwal <architagr@gmail.com>
2022-07-27 23:52:14 +0530

    2351(easy): First Letter to Appear Twice

--- easy_problems/first_letter_to_appear_twice/first_letter_to_appear_twice.go
+++ easy_problems/first_letter_to_appear_twice/first_letter_to_appear_twice.go
@@ -2,7 +2,7 @@ package first_letter_to_appear_twice
 
 func RepeatedCharacter(s string) byte {
 	mapS := make(map[byte]int)
-	ans := byte('a')
+	var ans byte = ' '
 	for i := 0; i < len(s); i++ {
 		if _, ok := mapS[s[i]]; ok {
 			ans = s[i]
