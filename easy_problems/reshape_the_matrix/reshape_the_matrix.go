d9a73665b3d6363c3f87681a85c7e381348b111d
Archit Agarwal <architagr@gmail.com>
2022-08-02 01:39:03 +0530

    566(easy): Reshape the Matrix

---
+++ easy_problems/reshape_the_matrix/reshape_the_matrix.go
@@ -0,0 +1,25 @@
+package reshape_the_matrix
+
+func MatrixReshape(mat [][]int, r, c int) [][]int {
+	arr := make([][]int, r)
+	row := make([]int, c)
+	if r*c != len(mat)*len(mat[0]) {
+		return mat
+	}
+	k, l := 0, 0
+
+	for i := 0; i < len(mat); i++ {
+		for j := 0; j < len(mat[0]); j++ {
+			if l == 0 {
+				row = make([]int, c)
+			}
+			row[l] = mat[i][j]
+			l++
+			if l == c {
+				arr[k] = row
+				l = 0
+			}
+		}
+	}
+	return arr
+}
