0bf18749e4f1f8b02a85be140921ad99a2101d76
Archit Agarwal <architagr@gmail.com>
2022-08-26 01:35:56 +0530

    1829(medium) Maximum XOR for Each Query

--- medium_problems/1801_1900/maximum_xor_for_each_query/maximum_xor_for_each_query.go
+++ medium_problems/1801_1900/maximum_xor_for_each_query/maximum_xor_for_each_query.go
@@ -2,15 +2,15 @@ package maximum_xor_for_each_query
 
 func GetMaximumXor(nums []int, maximumBit int) []int {
 	n := len(nums)
-	y := 0
+	prefixSum := make([]int, n)
+	prefixSum[0] = nums[0]
 	for i := 1; i < n; i++ {
-		y ^= nums[i]
+		prefixSum[i] = prefixSum[i-1] ^ nums[i]
 	}
 	x := (1 << maximumBit) - 1
 	result := make([]int, n)
 	for i := 0; i < n; i++ {
-		result[i] = y ^ x
-		y ^= nums[n-1-i]
+		result[i] = prefixSum[n-1-i] ^ x
 	}
 	return result
 }
