ff1e1ec17316f1b897dbe557fd03dfeea313b7d9
Archit Agarwal <architagr@gmail.com>
2022-09-08 00:45:16 +0530

    606(easy) Construct String from Binary Tree

---
+++ easy_problems/601_700/construct_string_from_binary_tree/construct_string_from_binary_tree.go
@@ -0,0 +1,27 @@
+package construct_string_from_binary_tree
+
+import "strconv"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func tree2str(root *TreeNode) string {
+	return parse(root)
+}
+
+func parse(node *TreeNode) string {
+	if node == nil {
+		return ""
+	}
+	s := strconv.Itoa(node.Val)
+	if node.Right != nil || node.Left != nil {
+		s += "(" + parse(node.Left) + ")"
+		if node.Right != nil {
+			s += "(" + parse(node.Right) + ")"
+		}
+	}
+	return s
+}
