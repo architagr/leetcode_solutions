165169bd7d8f221eb9d08be06336a9d3190abe9d
Archit Agarwal <architagr@gmail.com>
2025-02-24 21:31:10 +0530

    Search in a Binary Search Tree

---
+++ easy_problems/701_800/search_in_a_binary_search_tree/main.go
@@ -0,0 +1,20 @@
+package searchinabinarysearchtree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val > val {
+		return searchBST(root.Left, val)
+	}
+	if root.Val < val {
+		return searchBST(root.Right, val)
+	}
+	return root
+}
