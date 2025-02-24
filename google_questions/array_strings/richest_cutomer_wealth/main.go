978d7b41e4f202cb95d8677624380c808157caca
Archit Agarwal <architagr@gmail.com>
2025-02-24 21:38:08 +0530

    Richest Customer Wealth

---
+++ google_questions/array_strings/richest_cutomer_wealth/main.go
@@ -0,0 +1,16 @@
+package richestcutomerwealth
+
+func maximumWealth(accounts [][]int) int {
+
+	max := 0
+	for i := 0; i < len(accounts); i++ {
+		sum := 0
+		for j := 0; j < len(accounts[i]); j++ {
+			sum += accounts[i][j]
+		}
+		if sum > max {
+			max = sum
+		}
+	}
+	return max
+}
